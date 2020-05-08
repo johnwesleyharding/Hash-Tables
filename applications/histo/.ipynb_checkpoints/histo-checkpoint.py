5a849b91db6411938c5fe8d61c34962c19594751
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2020-05-08 15:35:23 -0600

    histo

--- applications/histo/.ipynb_checkpoints/histo-checkpoint.py
+++ applications/histo/.ipynb_checkpoints/histo-checkpoint.py
@@ -1,1 +1,29 @@
-# Implement me.
+import sys
+
+hashtable = {}
+doc = ''
+ignore = set('" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split())
+
+with open(sys.argv[1]) as f:
+    
+    for line in f:
+        
+        value = line.split()
+        
+        for word in value:
+            
+            simple = ''.join([char for char in word if char not in ignore]).lower()
+            
+            if simple not in hashtable:
+                
+                hashtable[simple] = '#'
+                
+            else:
+                
+                hashtable[simple] += '#'
+
+itemlist = list(hashtable.items())
+itemlist.sort(key = lambda x: x[1], reverse = True)
+
+for k, v in itemlist:
+    print(f'{k:10.9}{v}')
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,23 @@
+ignore = set('" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(' '))
+
 def word_count(s):
-    # Implement me.
+    
+    d = {}
+    s = s.split()
+    s = [''.join([char for char in word if char not in ignore]).lower() for word in s]
+    s = [word for word in s if word != '' and word != ' ']
+    
+    for word in s:
+        
+        if word in d:
+            
+            d[word] += 1
+        
+        else:
+            
+            d[word] = 1
+    
+    return d
 
 
 if __name__ == "__main__":
@@ -7,3 +25,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    
